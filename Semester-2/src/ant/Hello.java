5f15cc825ce544d53e5ae80602ac570470d9409b
Ciunas <ciunas.bennett@gmail.com>
2017-03-05 19:24:18 +0000

    bla

---
+++ Semester-2/src/ant/Hello.java
@@ -0,0 +1,14 @@
+package ant;
+
+public class Hello {
+
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
